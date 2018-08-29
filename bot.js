02bd29e2bb0dc4580cbc019e90152f8208a2c287
ahmedking12387 <33984820+ahmedking12387@users.noreply.github.com>
2018-08-29 16:29:37 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -26,6 +26,19 @@ client.on('ready', () => {
   console.log('')
 });
 
+
+client.on('message', message => {
+  if(message.content.startsWith(`${prefix}invites`)) {
+    message.guild.fetchInvites().then(invs => {
+      let user = message.mentions.users.first() || message.author
+      let personalInvites = invs.filter(i => i.inviter.id === user.id);
+      let inviteCount = personalInvites.reduce((p, v) => v.uses + p, 0);
+message.channel.send(`${user} has ${inviteCount} invites.`);
+});
+  }
+});
+
+
 client.on('message', function(message) {
 	const myID = "279194403564814336";
     let args = message.content.split(" ").slice(1).join(" ");
